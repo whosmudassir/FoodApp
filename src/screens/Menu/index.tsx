42538f5e06e43a2aa10b2a2728321d29d51e4d82
Mudassir Khan <deadsoulartist@gmail.com>
2021-09-14 17:48:33 +0530

    search bar

--- src/screens/Menu/index.tsx
+++ src/screens/Menu/index.tsx
@@ -12,18 +12,62 @@ import {data} from '../../constants/mock-data';
 import Card from '../../components/Card';
 import Footer from '../../components/Footer';
 import {styles} from './styles';
+import CardItem from '../../components/CardItem';
 
 export default function Menu() {
   const [menu, setMenu] = useState(data.categories);
+  const [searchText, setSearchText] = useState('');
+
+  const food = [];
+
+  data.categories.map(main => (
+    <>
+      {main.items
+        ? main.items.map(item => {
+            return food.push(item);
+          })
+        : main.subCategories.map(item => (
+            <p>
+              {item.items.map(item => {
+                return food.push(item);
+              })}
+            </p>
+          ))}
+      <br />
+    </>
+  ));
+
+  console.log('food->', food);
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <ScrollView>
         <TextInput
           style={styles.input}
           placeholder="Steaks, Entress, Drinks etc."
+          value={searchText}
+          onChangeText={value => setSearchText(value)}
         />
 
-        <FlatList data={menu} renderItem={({item}) => <Card main={item} />} />
+        {searchText === '' ? (
+          <FlatList data={menu} renderItem={({item}) => <Card main={item} />} />
+        ) : (
+          food
+            .filter(item => {
+              if (item.name.toLowerCase().includes(searchText.toLowerCase())) {
+                console.log('this item->', item);
+                return item;
+              } else {
+                console.log('nothing');
+                return null;
+              }
+            })
+            .map(item => {
+              console.log('map->', item);
+
+              return <CardItem item={item} />;
+            })
+        )}
 
         <Footer />
       </ScrollView>
